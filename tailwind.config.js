a0986e3978f00e36d2fb10e859b97ef1f8fe276e
Eshwar Reddy <eshwarreddy.thummala@gmail.com>
2024-05-20 13:04:49 -0700

    Added logo to the title page

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,14 +28,14 @@ module.exports = {
         textGreen: "#262626",
         textLight: "#797979",
         textDark: "#262626",
-        hoverColor: "rgba(100,255,218,0.1)",
+        hoverColor: "rgba(226,226,226,0.1)",
       },
       fontFamily: {
         bodyFont: ["Montserrat", "sans-serif"],
         titleFont: ["Inter", "sans-serif"],
       },
       boxShadow: {
-        navbarShadow: " 0 10px 30px -10px rgba(2,12,27,0.7)",
+        navbarShadow: " 0 10px 10px -10px rgba(2,12,27,0.7)",
       },
     },
   },
