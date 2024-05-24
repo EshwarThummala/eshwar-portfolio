62f16050a177f3c4ff37e5e9a1edf8adeb83aab6
Eshwar Reddy <eshwarreddy.thummala@gmail.com>
2024-05-24 16:06:26 -0700

    Added more skills

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import ArchiveCard from "./ArchiveCard";
 import { motion } from "framer-motion";
-import { RxOpenInNewWindow } from "react-icons/rx";
-import { SiFlask } from "react-icons/si";
-import { FaPython, FaReact, FaJs } from "react-icons/fa";
-import { BiLogoSpringBoot, BiLogoTypescript, BiNetworkChart  } from "react-icons/bi";
-import { FaAws, FaC, FaJava } from "react-icons/fa6";
+import { SiFlask, SiKibana, SiPowerbi, SiTensorflow } from "react-icons/si";
+import { FaPython, FaReact, FaJs, FaAndroid, FaGitAlt } from "react-icons/fa";
+import {
+  BiLogoSpringBoot,
+  BiNetworkChart,
+} from "react-icons/bi";
+import { FaAws, FaJava } from "react-icons/fa6";
+import { CgCPlusPlus } from "react-icons/cg";
 import { BsRobot } from "react-icons/bs";
 import { RiComputerLine } from "react-icons/ri";
-import { CiCloudRainbow } from "react-icons/ci";
+import { CiCloudRainbow, CiMobile3 } from "react-icons/ci";
 import { FiDatabase } from "react-icons/fi";
 import { GrDocumentImage } from "react-icons/gr";
 import { VscSettings } from "react-icons/vsc";
-import { DiScrum } from "react-icons/di";
+import { DiScrum, DiMysql } from "react-icons/di";
 import { MdOutlineInsertChart } from "react-icons/md";
-import { GiSpanner } from "react-icons/gi";
-
+import { PiFileSqlThin } from "react-icons/pi";
 
 
 const Archive = () => {
@@ -23,11 +25,11 @@ const Archive = () => {
   const firstSkills = [
     {
       name: "Front-End",
-      icon: RiComputerLine
+      icon: RiComputerLine,
     },
     {
       name: "API's",
-      icon: VscSettings
+      icon: VscSettings,
     },
     {
       name: "Database",
@@ -50,8 +52,8 @@ const Archive = () => {
       icon: GrDocumentImage,
     },
     {
-      name: "Testing",
-      icon: GiSpanner,
+      name: "Mobile App Development",
+      icon: CiMobile3,
     },
     {
       name: "Agile Framework",
@@ -60,7 +62,7 @@ const Archive = () => {
     {
       name: "Data Visualization",
       icon: MdOutlineInsertChart,
-    }
+    },
   ];
   const secondSkills = [
     {
@@ -80,14 +82,6 @@ const Archive = () => {
       icon: FaAws,
     },
     {
-      name: "ML",
-      icon: BsRobot,
-    },
-    {
-      name: "ML",
-      icon: BsRobot,
-    },
-    {
       name: "Java",
       icon: FaJava,
     },
@@ -100,25 +94,49 @@ const Archive = () => {
       icon: FaJs,
     },
     {
-      name: "TypeScript",
-      icon: BiLogoTypescript,
+      name: "Programming",
+      icon: CgCPlusPlus,
     },
     {
-      name: "Programming",
-      icon: FaC,
+      name: "SQL",
+      icon: PiFileSqlThin,
+    },
+    {
+      name: "Power BI",
+      icon: SiPowerbi,
+    },
+    {
+      name: "MySQL",
+      icon: DiMysql,
+    },
+    {
+      name: "Git",
+      icon: FaGitAlt,
+    },
+    {
+      name: "Android",
+      icon: FaAndroid
+    },
+    {
+      name: "Kibana",
+      icon: SiKibana
+    },
+    {
+      name: "Tensorflow",
+      icon: SiTensorflow
     }
   ];
   return (
     <div className="max-w-contentContainer mx-auto px-4 py-24">
       <div className="w-full flex flex-col items-center">
         <h2 className="text-3xl font-titleFont font-semibold">
-          Technical Skills
+          Different Technology Domains I worked
         </h2>
-        <p className="text-sm font-titleFont text-textGreen">
+        {/*<p className="text-sm font-titleFont text-textGreen">
           ofcourse there are many...
-        </p>
+  </p>*/}
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-6 mt-10 lgl:px-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-6 mt-10 lgl:px-10">
         {firstSkills.map((skill) => (
           <ArchiveCard
             key={skill.name}
@@ -149,14 +167,14 @@ const Archive = () => {
             onClick={() => setShowMore(false)}
             className="w-36 h-12 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
           >
-            Show Less
+            Hide Specific Skills
           </button>
         ) : (
           <button
             onClick={() => setShowMore(true)}
             className="w-36 h-12 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
           >
-            Show More
+            Specific Skills
           </button>
         )}
       </div>
