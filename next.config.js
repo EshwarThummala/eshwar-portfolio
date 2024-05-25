a0bdcf9c16229483aae63c7a5d983adbedf1d629
Eshwar Reddy <eshwarreddy.thummala@gmail.com>
2024-05-25 12:04:56 -0700

    Modified next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
+  output: 'export',
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
