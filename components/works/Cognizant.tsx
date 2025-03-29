59dc53a7155e9604136cc648c3e173e2d183bb5a
EshwarThummala <eshwarreddy.thummala@gmail.com>
2025-03-29 16:57:07 -0700

    Work update

--- components/works/Cognizant.tsx
+++ components/works/Cognizant.tsx
@@ -10,7 +10,7 @@ const Cognizant = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Programmer Analyst Trainee
+        Programmer Analyst
         <span className="text-textGreen tracking-wide">@Cognizant</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
